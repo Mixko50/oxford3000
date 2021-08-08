7347748e2acd8512d8ea8c90ccfccbfd5cbe0fd0
Apisit Maneerat <Mixko50@users.noreply.github.com>
2021-08-08 17:32:09 +0700

    Update config.js

--- Backend/src/utils/config.js
+++ Backend/src/utils/config.js
@@ -1,6 +1,10 @@
+const { dev } = require("./enviroment");
+
 module.exports = {
     jwt_secret: "Mixko50LovesOkashi",
-    redirect_uri: "http://localhost:8080/google/auth",
+    redirect_uri: dev
+        ? "http://localhost:8080/google/auth"
+        : "https://oxford3000.mixko.ml/google/auth",
     google_client_secret: "9Ya_pUmeUBDkubE55N3GiZ5A",
     google_client_id:
         "198546295289-ci6a3j9rmdcre8gmi12tbn4sarph4jjc.apps.googleusercontent.com",
